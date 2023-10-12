db5d96f254860f5423975b31d8865844acf413d8
Faiem-qy <f.quanyoung@gmail.com>
2023-10-11 23:36:23 -0400

    create seeds for database tables

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,7 @@
+
+-- categories 
+INSERT INTO categories (category_name) VALUES ('HTML');
+INSERT INTO categories (category_name) VALUES ('CSS');
+INSERT INTO categories (category_name) VALUES ('Javascript');
+INSERT INTO categories (category_name) VALUES ('React');
+INSERT INTO categories (category_name) VALUES ('Node');
---
+++ db/seeds/06_ratings.sql
@@ -0,0 +1,14 @@
+
+
+-- ratings
+
+INSERT INTO ratings (user_id, resource_id) VALUES (1, 1);
+INSERT INTO ratings (user_id, resource_id) VALUES (2, 2);
+INSERT INTO ratings (user_id, resource_id) VALUES (3, 3);
+INSERT INTO ratings (user_id, resource_id) VALUES (4, 4);
+INSERT INTO ratings (user_id, resource_id) VALUES (5, 5);
+INSERT INTO ratings (user_id, resource_id) VALUES (6, 6);
+INSERT INTO ratings (user_id, resource_id) VALUES (1, 2);
+INSERT INTO ratings (user_id, resource_id) VALUES (3, 4);
+INSERT INTO ratings (user_id, resource_id) VALUES (2, 3);
+INSERT INTO ratings (user_id, resource_id) VALUES (4, 1);
