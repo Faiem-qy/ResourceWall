78b170700c59debd47c61c4ac497bddbfcefd041
Faiem-qy <f.quanyoung@gmail.com>
2023-10-14 14:52:15 -0400

    add route for profile page and link to users-show.ejs

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,53 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const { getOneUser, updateUserProfile } = require('../db/queries/users');
 
 router.get('/', (req, res) => {
   res.render('users');
+});
+
+router.get('/:id', (req, res) => {
+  const userId = req.params.id;
+
+  getOneUser(userId)
+    .then(user => {
+      const templateVars = {
+        userId,
+        user
+      };
+      console.log(user);
+      res.render('users-show', templateVars);
+    })
+    .catch(error => {
+      res.status(500).json({ error: err.message });
+    });
+
+});
+
+router.post('/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const user = {
+    "first_name": "Lorie",
+    "last_name": "Saladine",
+    "email": "lorie@mail.com",
+    "password": "1",
+    "profile_picture": "https://images.pexels.com/photos/2853198/pexels-photo-2853198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "handle": "leSalad",
+    "bio": "ah word"
+  };
+  console.log(user);
+
+  updateUserProfile(userId, user)
+    .then(user => {
+      console.log(user);
+      res.redirect(`/users/${userId}`);
+    })
+    .catch(err => {
+      console.log({ error: err.message });
+    });
+
 });
 
 module.exports = router;
